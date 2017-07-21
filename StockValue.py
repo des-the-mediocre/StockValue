3a4934196f98d32528c7e1096af07d53c7de8911
Andreas Nesse <andreas.nesse@lyse.net>
2017-07-21 20:02:22 +0200

    Update StockValue.py
    Now gets current data in addition to historic

--- StockValue.py
+++ StockValue.py
@@ -8,16 +8,15 @@ import pandas_datareader as reader
 import datetime
 from datetime import timedelta
 
-version = '0.2'
+version = '0.3'
 
 today = datetime.date.today()
 yesterday = today - timedelta(days=1)
 
-#method for finding values of stocks inputted by the user
-def getStockValue(today, yesterday):
+#method for 
+def getCompanies():
 	names = []
 	i = 1
-	alldata = pd.DataFrame()
 	print('Enter the companies you wish to check. \nLeave blank when finished.\n')
 	while 1:
 		company = input('Company nr.%d: ' %i)
@@ -25,19 +24,35 @@ def getStockValue(today, yesterday):
 			break
 		names.append(company.upper())
 		i = i+1
+	return names
+
+def getStockValue(names, today, yesterday):
+	alldata = pd.DataFrame()
+	alldata = reader.get_quote_google(names)
+	#alldata = alldata['time'].dt.time
+	return alldata
+
+def getHistoric(names):
+	alldata = pd.DataFrame()
 	for name in names:
 		stockdata = reader.DataReader(name, "google", yesterday, today)
 		stockdata = stockdata.tail(1)
 		alldata = alldata.append(stockdata)
-	#list of columns to change order and add company names
-	col = ['Company']
-	col.extend(alldata.columns.tolist())
+	#col = ['Company']
+	#col.extend(alldata.columns.tolist())
 	alldata = alldata.assign(Company=names)
-	return alldata[col]
+	#alldata = alldata[col]
+	alldata = alldata.set_index(['Company'], append=True)
+	return alldata
 
 print('\ndes the mediocre\'s StockValue \nv' + version + '\n')
-stockvalues = getStockValue(today, yesterday)
+#companies = getCompanies()
+companies = ['AAPL','MSFT']
+stockvalues = getStockValue(companies, today, yesterday)
+historicvalues = getHistoric(companies)
 
 print('\n')
 print(stockvalues)
+print('\n')
+print(historicvalues)
 print('\n')
