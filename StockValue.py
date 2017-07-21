89d3db1f598435a5f13ad4668e293dc092290abf
Andreas Nesse <andreas.nesse@lyse.net>
2017-07-21 04:01:19 +0200

    Update StockValue.py
    Now accepts list of companies, and displays company symbols

--- StockValue.py
+++ StockValue.py
@@ -1,23 +1,43 @@
+'''
+Program takes input from users and makes a list of symbols representing stocks.
+The program returns the values for the stocks the last day the exchange was open.
+'''
+
 import pandas as pd
 import pandas_datareader as reader
 import datetime
 from datetime import timedelta
 
-version = '0.1'
+version = '0.2'
 
 today = datetime.date.today()
 yesterday = today - timedelta(days=1)
 
+#method for finding values of stocks inputted by the user
 def getStockValue(today, yesterday):
-	print('Please enter the name of the stock:')
-	name = input()
-	name = name.upper()
-	value = reader.DataReader(name, "google", yesterday, today)
-	value = value.tail(1)
-	return value
+	names = []
+	i = 1
+	alldata = pd.DataFrame()
+	print('Enter the companies you wish to check. \nLeave blank when finished.\n')
+	while 1:
+		company = input('Company nr.%d: ' %i)
+		if company == '':
+			break
+		names.append(company.upper())
+		i = i+1
+	for name in names:
+		stockdata = reader.DataReader(name, "google", yesterday, today)
+		stockdata = stockdata.tail(1)
+		alldata = alldata.append(stockdata)
+	#list of columns to change order and add company names
+	col = ['Company']
+	col.extend(alldata.columns.tolist())
+	alldata = alldata.assign(Company=names)
+	return alldata[col]
 
-print('\nStockValue version ' + version + '\n')
-value = getStockValue(today, yesterday)
+print('\ndes the mediocre\'s StockValue \nv' + version + '\n')
+stockvalues = getStockValue(today, yesterday)
 
-print(value)
+print('\n')
+print(stockvalues)
 print('\n')
